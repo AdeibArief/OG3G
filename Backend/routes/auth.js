e6506768939529738b8cd5d7cb8cc1d26e27fa14
AdeibArief <sadeib3@gmail.com>
2026-04-30 16:36:21 +0530

    fix: update SameSite attribute for cookie to 'none'

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -13,7 +13,7 @@ const sendTokenCookie = (res, userId) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 };
